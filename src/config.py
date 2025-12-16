9fc61857d5a9393ae2560c6d86ef47c7556b02c2
barkin <63337752+barkinadiguzel@users.noreply.github.com>
2025-12-16 11:23:32 +0300

    Create config.py

---
+++ src/config.py
@@ -0,0 +1,11 @@
+# video settings
+TIME_STEPS = 10          # in paper T = 10
+FRAME_HEIGHT = 227
+FRAME_WIDTH = 227
+CHANNELS = 1             # grayscale
+
+# model
+SPATIAL_CHANNELS = [128, 256]
+
+# runtime
+DEVICE = "cuda" 
